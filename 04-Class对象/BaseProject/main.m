3cb65589f18ae75e27dee05eb9b02c76b20005ea
ZIYOCHIRO <ziyochiro@gmail.com>
2021-03-17 21:53:00 +0800

    update

---
+++ 04-Class对象/BaseProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BaseProject
+//
+//  Created by ruixinyi on 2021/3/1.
+//
+
+#import <Foundation/Foundation.h>
+#import "EmrMethod.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
